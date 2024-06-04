d855975c9b7146be82048265066923bde1694fd0
Arthur Zhassymov <zhassymov>
2024-06-04 11:09:08 +0500

    feat: add Main

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Hero from "./components/Hero";
 import MenuCategories from "./components/MenuCategories";
 import MenuCards from "./components/MenuCards";
 import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 
 export default function App() {
     return (
         <>
             <Header/>
-            <Hero/>
-            <MenuCategories/>
-            <MenuCards/>
+            <Main>
+                <Hero/>
+                <MenuCategories/>
+                <MenuCards/>
+            </Main>
             <Footer/>
         </>
     )
