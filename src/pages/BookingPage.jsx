c6ffd5c5a5974d9e12852cfbace65ab6b2938841
Arthur Zhassymov <zhassymov>
2024-06-04 11:33:39 +0500

    feat: add BookingPage layout

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,5 +1,18 @@
+import Container from "../ui/Container";
+import Title from "../ui/Title";
+import Subtitle from "../ui/Subtitle";
+import BookingForm from "../components/BookingForm";
+
 export default function BookingPage() {
     return (
-        <div></div>
+        <div className="bg-brand-green text-white">
+            <Container>
+                <article className="text-white px-4 py-8">
+                    <Title>Little Lemon</Title>
+                    <Subtitle>Chicago</Subtitle>
+                    <BookingForm/>
+                </article>
+            </Container>
+        </div>
     )
 }
